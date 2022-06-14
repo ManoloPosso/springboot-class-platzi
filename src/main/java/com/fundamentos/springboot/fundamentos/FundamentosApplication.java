397d11ad59f06ccf15ec11b6ecd8b5212300d369
Manuel Posso <mfposso0911@gmail.com>
2022-06-14 14:12:23 -0500

    Inyeccion de Dependencias y su respectiva configuracion

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,42 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+
+	//INYECTAR DEPENDENCIA
+	private ComponentDependency componentDependency;
+	private MyBean myBean;
+	private MyBeanWithDependency myBeanWithDependency;
+
+	//La anotacion @Qualifier, nos permite especificar què dependencia es la que estamos inyectando
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency){
+
+		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithDependency = myBeanWithDependency;
+
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	//Este metodo se trae de la implementacion de la clase CommandLineRunner, y sirve para estipular què metodos se ejecutaran
+	//cuando se corra el proyecto
+	@Override
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+		myBean.print();
+		myBeanWithDependency.printWithDependency();
+
+	}
 }
---
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBean2Implement.java
@@ -0,0 +1,8 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+public class MyBean2Implement implements MyBean{
+    @Override
+    public void print() {
+        System.out.println("HOLA DESDE MI IMPLEMENTACION 2 DEL BEAN 2");
+    }
+}
