27102fdda15bb9cd7f5086eb38fe0e0489d04fa3
Manuel Posso <mfposso0911@gmail.com>
2022-07-12 15:46:13 -0500

    Continuacion de POJO y Mapeo de Entidades con JPA

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,9 @@ import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +15,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	//Usando la libreria commons de Apache (Log) podemos mostrar detalladamente en la consola los errores
+	//Incluso manejando try - catch
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+
 	//INYECTAR DEPENDENCIA
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
@@ -19,13 +26,16 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	private MyBeanWithProperties myBeanWithProperties;
 
+	private UserPojo userPojo;
+
 	//La anotacion @Qualifier, nos permite especificar què dependencia es la que estamos inyectando
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 
 
 	}
@@ -42,6 +52,21 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println("******  " + userPojo.getEmail() + " - " + userPojo.getPassword());
+
+		try {
+
+			int value = 10/10;
+
+			LOGGER.debug("#####Mi valor es: " + value);
+
+		}catch (Exception ex){
+
+			LOGGER.error("######Esto es un error al dividir por 0" + ex.getStackTrace());
+
+		}
+
+		;
 
 	}
 }
